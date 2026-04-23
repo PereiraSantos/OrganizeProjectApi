52083f06aef9cd7fbfdb50949f622931c2d93b35
marciano <marciano@marciano>
2026-04-23 20:50:18 -0300

    Removido pasta que foi comitada

--- dist/routes/projectRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const projectController_1 = require("../controllers/projectController");
-const router = (0, express_1.Router)();
-router.post('/', projectController_1.createProject);
-router.get('/', projectController_1.getProjects);
-exports.default = router;
--- dist/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.post('/', userController_1.createUser);
-router.post('/auth', userController_1.auth);
-router.get('/', userController_1.getUsers);
-exports.default = router;
